37f5849c8ae88926ee11bca96dcee0ad207f06ea
yerko <yerkoisaack@gmail.com>
2018-11-02 11:05:52 -0300

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_IRI;
+
 }
 process.env.URLDB = urlDB;
